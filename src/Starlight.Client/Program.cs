77042a0ded7a10a2f350de9aee58db164a287c0d
Zynato <z@mail.eclipseorigins.com>
2020-07-23 20:53:47 -0400

    Revert "Revert "Create a reusable class for surfaces""
    This reverts commit da5632c7b5a269f537eafe039d7352a6190ffb99.

--- src/Starlight.Client/Program.cs
+++ src/Starlight.Client/Program.cs
@@ -1,4 +1,5 @@
 ﻿using SDL2;
+using Starlight.Client.Rendering;
 using System;
 using System.IO;
 using static SDL2.SDL;
@@ -30,10 +31,7 @@ namespace Starlight.Client
 
             var workingDirectory = Directory.GetCurrentDirectory();
 
-            var surfacePtr = IMG_Load(Path.Combine(workingDirectory, "Assets", "graphics", "items", "1.png"));
-            var surface = surfacePtr.AsStruct<SDL_Surface>();
-
-            var image = SDL_CreateTextureFromSurface(renderer, surfacePtr);
+            var surface = Surface.Load(renderer, Path.Combine(workingDirectory, "Assets", "graphics", "items", "1.png"));
 
 
             var isRunning = true;
@@ -68,19 +66,19 @@ namespace Starlight.Client
                 {
                     x = 0,
                     y = 0,
-                    w = surface.w,
-                    h = surface.h
+                    w = surface.Width,
+                    h = surface.Height
                 };
 
                 var dstRect = new SDL_Rect()
                 {
                     x = 200,
                     y = 200,
-                    w = surface.w,
-                    h = surface.h
+                    w = surface.Width,
+                    h = surface.Height
                 };
 
-                SDL_RenderCopy(renderer, image, ref srcRect, ref dstRect);
+                SDL_RenderCopy(renderer, surface.TextureHandle, ref srcRect, ref dstRect);
 
                 SDL_RenderPresent(renderer);
             }
