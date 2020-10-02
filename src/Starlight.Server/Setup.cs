edc31f6ebfb5c2ba5e991be78d8e2170beae1a4e
Julius ter Pelkwijk <pelkwijk@gmail.com>
2020-10-02 09:06:24 +0200

    Database migration quickfix

--- src/Starlight.Server/Setup.cs
+++ src/Starlight.Server/Setup.cs
@@ -4,14 +4,24 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
 
 namespace Starlight.Server
 {
     public static class Setup
     {
         public static void RunSetup(Configuration configuration) {
+            
             var dbContext = DbContextFactory.CreateApplicationDbContext(configuration.ConnectionString);
-
+            Console.WriteLine("Updating database...");
+            try {
+                dbContext.Database.Migrate();
+            }
+            catch {
+                Console.WriteLine("Error encountered during update!");
+                return;
+            };
             if (!dbContext.Maps.Any()) {
                 Console.WriteLine("Creating initial map.");
 
