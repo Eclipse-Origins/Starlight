bf3512f9af9a564a4854b2185670831f9e97eeda
Julius ter Pelkwijk <pelkwijk@gmail.com>
2020-10-09 14:45:14 +0200

    Adding editor to commandline

--- src/Starlight.Editors/Program.cs
+++ src/Starlight.Editors/Program.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.CommandLine.DragonFruit;
 
 namespace Starlight.Editors
 {
@@ -13,23 +14,29 @@ namespace Starlight.Editors
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
+        /// <param name="debug">To turn debugging on or off</param>
+        /// <param name="server">IP address of server</param>
+        /// <param name="port">Portnumber of server</param>
+        /// <param name="language">Language being used</param>
         [STAThread]
-        static void Main() {
-            Log.Logger = (ILogger)new LoggerConfiguration()
-                .WriteTo.File("editors.log")
+        static void Main(bool debug = false, string server = null, int port = 1338, string language = "en-us") {
 #if DEBUG
-                .WriteTo.Console()
-                .MinimumLevel.Debug()
+            debug = true;
+            server = "localhost";
 #endif
+            if (debug) {
+                Log.Logger = (ILogger)new LoggerConfiguration()
+                .WriteTo.File("editors.log")
                 .CreateLogger();
+            }
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
             var starlightContext = new StarlightContext(Directory.GetCurrentDirectory());
 
-            starlightContext.LoadContent();
-            starlightContext.Connect();
+            starlightContext.LoadContent(language);
+            starlightContext.Connect(server,port);
 
             starlightContext.FormContainer.ChangeForm<LoginForm>();
 
